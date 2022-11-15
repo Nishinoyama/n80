bfd13d0c784aaea779f65dc884225d1eec161e34
Nishinoyama <begoingto1414@gmail.com>
2022-11-15 14:39:13 +0900

    new: 16bit register dividable

--- src/register.rs
+++ src/register.rs
@@ -3,13 +3,16 @@ use std::mem::transmute;
 pub trait Register16 {
     fn write(&mut self, bits: u16);
     fn load(&self) -> u16;
+}
+
+pub trait Register16Dividable: Register16 {
     fn write_l(&mut self, bits: u8);
     fn load_l(&self) -> u8;
     fn write_h(&mut self, bits: u8);
     fn load_h(&self) -> u8;
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct R16Bits {
     bits: u16,
 }
@@ -23,6 +26,14 @@ impl Register16 for R16Bits {
     fn load(&self) -> u16 {
         self.bits
     }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct R16Bits8Bits {
+    bits: u16,
+}
+
+impl Register16Dividable for R16Bits8Bits {
     fn write_l(&mut self, bits: u8) {
         unsafe {
             *transmute::<&u16, *mut u8>(&self.bits).add(1) = bits;
@@ -43,13 +54,24 @@ impl Register16 for R16Bits {
     }
 }
 
+impl Register16 for R16Bits8Bits {
+    #[inline]
+    fn write(&mut self, bits: u16) {
+        self.bits = bits
+    }
+    #[inline]
+    fn load(&self) -> u16 {
+        self.bits
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use crate::register::{R16Bits, Register16};
+    use crate::register::{R16Bits8Bits, Register16, Register16Dividable};
 
     #[test]
     fn set_get_register() {
-        let mut reg = R16Bits::default();
+        let mut reg = R16Bits8Bits::default();
         reg.write(0x0123_u16.to_be());
         assert_eq!(reg.load().to_be(), 0x0123);
         assert_eq!(reg.load_h(), 0x01);
